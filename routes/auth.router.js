527675de7bfb998b69b63968ecd06ff82bd2b379
Y3ASIN <yeasinarafat.wa@gmail.com>
2025-03-16 21:36:23 +0600

    Update: Endpoint correction.

--- routes/auth.router.js
+++ routes/auth.router.js
@@ -7,6 +7,6 @@ const authRouter = express.Router();
 
 authRouter.post("/sign-up", signUp);
 authRouter.post("/sign-in", signIn);
-authRouter.get("sign-out", signOut);
+authRouter.get("/sign-out", signOut);
 
 export default authRouter;
